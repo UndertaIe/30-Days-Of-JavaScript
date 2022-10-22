576eebd5f58816a1c37d695cea93e4a5e9f55637
UndertaIe <n604712799@gmail.com>
2022-10-22 14:33:35 +0800

    finish day09 and day10

--- 10_Day_Sets_and_Maps/10_day_starter/scripts/main.js
+++ 10_Day_Sets_and_Maps/10_day_starter/scripts/main.js
@@ -1,2 +1,60 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+
+
+const sets = new Set()
+console.log(sets)
+
+arr = [1,2,3,1]
+const sets2 = new Set(arr)
+console.log(sets2)
+
+sets.add(1)
+sets.add('AB')
+console.log(sets)
+
+console.log(sets.keys())
+console.log(sets.values())
+
+sets.delete('AB')
+console.log(sets)
+
+sets.clear()
+console.log(sets)
+
+
+
+const map = new Map()
+console.log(map)
+
+countries2 = new Set(
+    [['Finland', 'Helsinki'],
+    ['Sweden', 'Stockholm'],
+    ['Norway', 'Oslo']
+]);
+// countries2= [
+//     ['Finland', 'Helsinki'],
+//     ['Sweden', 'Stockholm'],
+//     ['Norway', 'Oslo'],
+//   ]
+
+const map2 = new Map(countries2)
+console.log(map2)
+console.log(map2.size)
+
+map2.set('1','2')
+console.log(map2)
+
+map2.set(1,'2')
+console.log(map2)
+
+let v = map2.get(1)
+console.log("map2[1]: ", v)
+console.log(map2[1])
+
+map2.delete(1)
+map2.delete('1')
+map2.delete("Norway")
+console.log(map2)
+
+map2.clear()
+console.log(map2)
+
--- 11_Day_Destructuring_and_spreading/11_day_starter/scripts/main.js
+++ 11_Day_Destructuring_and_spreading/11_day_starter/scripts/main.js
@@ -1,2 +1,57 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+// console.log(countries)
+// alert('Open the console and check if the countries has been loaded')
+
+const numbers = [1, 2, 3]
+
+let [numOne, numTwo, numThree] = numbers
+console.log(numOne, numTwo, numThree)
+
+let v1,v2,v3 = numbers
+console.log(v1,v2,v3)
+
+let [vv1,vv2 ] = numbers
+console.log(vv1,vv2)
+
+let [numOne2, , numThree2] = numbers //2 is omitted
+console.log(numOne2, numThree2)
+
+const names = [undefined, 'Brook', 'David']
+let [
+  firstPerson = 'Asabeneh',
+  secondPerson,
+  thirdPerson,
+  fourthPerson = 'John'
+] = names
+console.log(firstPerson, secondPerson, thirdPerson, fourthPerson)  
+
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let [num1, num2, num3, ...rest] = nums
+console.log(num1, num2, num3)
+console.log(rest)
+
+let [num11, num22, num33, ...rest2] = nums
+
+console.log(num11, num22, num33)
+console.log(rest2)
+
+const evens = [0, 2, 4, 6, 8, 10]
+const evenNumbers = [...evens]
+const odds = [1, 3, 5, 7, 9]
+const oddNumbers = [...odds]
+const wholeNumbers = [...evens, ...odds]
+console.log(evenNumbers)
+console.log(oddNumbers) 
+console.log(wholeNumbers)
+
+
+const user = {
+    name:'Asabeneh',
+    title:'Programmer',
+    country:'Finland',
+    city:'Helsinki'
+  }
+const copiedUser = {...user}
+console.log(copiedUser)
+
+const copiedUserA = {...user, title:'instructor'}
+console.log(copiedUserA)
